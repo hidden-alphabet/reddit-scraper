29d19e7e8b23f7bfe94796cedd989c0dc8bcf8c1
macscheffer <43249742+macscheffer@users.noreply.github.com>
2019-06-07 16:02:26 -0400

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -1,20 +1,21 @@
 import praw
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import pandas as pd
+from sqlalchemy import create_engine
+nltk.download('vader_lexicon')
 
-import csv
+engine = create_engine(
+    'postgresql://HIDDENALPHABET:password@hiddenalphabet-db.ceqxyonbodui.us-east-1.rds.amazonaws.com:5432/HiddenAlphabet')
+
 
 REDDIT_CLIENT_ID = "VXYCrQAymaBrLQ"
 REDDIT_CLIENT_SECRET = "0Wwvv9zptchx7iqweK52cvbtX4A"
 REDDIT_USER_AGENT = "WSB"
 
 reddit = praw.Reddit(client_id=REDDIT_CLIENT_ID,
-                     client_secret = REDDIT_CLIENT_SECRET, 
+                     client_secret = REDDIT_CLIENT_SECRET,
                      user_agent=REDDIT_USER_AGENT)
 
 reddit_financials = reddit.subreddit(
         'wallstreetbets+investing+stocks+options+SecurityAnalysis+RobinHood+tradevol+thewallstreet')
-
-for comment in reddit_financials.stream.comments():
-	row = [comment.id, comment.body, comment.created_utc, comment.subreddit_id]
-	with open('comments.csv', 'w') as writeFile:
-		writer = csv.writer(writeFile)
-		writer.writerow(row)
